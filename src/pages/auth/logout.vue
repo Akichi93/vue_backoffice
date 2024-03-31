df0ecc25922c7ecf17562882aebb937746214ab7
Akichi93 <jeanphilippeakichi@gmail.com>
2024-03-31 17:08:21 +0000

    update .env.example

--- src/pages/auth/logout.vue
+++ src/pages/auth/logout.vue
@@ -1,45 +1,40 @@
-<template>
-
-</template>
+<template></template>
 <script>
 import AppStorage from "../../db/AppStorage.js";
-import syncservice from "../../services/syncService.js"
+import syncservice from "../../services/syncService.js";
 export default {
-    data() {
+  data() {
     return {
       errors: {},
       isConnected: false,
     };
   },
-    created() {
-        AppStorage.clear()
-                AppStorage.deleteIndexedDB()
-                this.$router.push({ name: 'welcome' })
-        // this.logout();
+  created() {
+    AppStorage.clear();
+    AppStorage.deleteIndexedDB();
+    this.$router.push({ name: "welcome" });
+    // this.logout();
+  },
+  methods: {
+    async logout() {
+      const apiUrl = process.env.VUE_APP_API_BASE_URL;
+
+      // Then use it in your fetch request
+      const response = await fetch(`${apiUrl}/api/check-internet-connection`);
+
+      const data = await response.json();
+      this.isConnected = data.connected;
+      if (this.isConnected) {
+        await syncservice.checkAndSyncData();
+
+        AppStorage.clear();
+        AppStorage.deleteIndexedDB();
+        this.$router.push({ name: "welcome" });
+      } else {
+        console.log("Veuillez vous connectez à internet");
+      }
     },
-    methods: {
-        async logout() {
-            const response = await fetch(
-                "/api/check-internet-connection"
-            );
-
-            const data = await response.json();
-            this.isConnected = data.connected;
-            if (this.isConnected) {
-
-                await syncservice.checkAndSyncData();
-
-                AppStorage.clear()
-                AppStorage.deleteIndexedDB()
-                this.$router.push({ name: 'welcome' })
-            } else {
-                console.log('Veuillez vous connectez à internet')
-            }
-
-        }
-    },
-
-
-}
+  },
+};
 </script>
 <style scoped></style>
